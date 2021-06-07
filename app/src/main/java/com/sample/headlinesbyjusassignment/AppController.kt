d526920ce3a8fae6ce58d4659f2acbebc176a872
HemaSathiz <42398958+HemaSathiz@users.noreply.github.com>
2021-06-07 16:42:06 +0530

    MVVM project setup
    Done with mvvm project setup

---
+++ app/src/main/java/com/sample/headlinesbyjusassignment/AppController.kt
@@ -0,0 +1,7 @@
+package com.sample.headlinesbyjusassignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppController : Application()
--- app/src/main/java/com/sample/headlinesbyjusassignment/ui/MainActivity.kt
+++ app/src/main/java/com/sample/headlinesbyjusassignment/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sample.headlinesbyjusassignment
+package com.sample.headlinesbyjusassignment.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sample.headlinesbyjusassignment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
